b99aca9f5546ec32dae1991191e8d3ca7b08ea08
eugene94 <eugenekang94@gmail.com>
2019-08-15 08:51:15 +0900

    Feat: location / filter state로 관리

--- src/components/FoodPage.js
+++ src/components/FoodPage.js
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 
 import { FoodContext } from '../store/FoodStore';
 import FoodList from './FoodList';
 
 const FoodPage = () => {
+  const [location, setLocation] = useState('냉장');
+  const [filter, setFilter] = useState('전체');
+
   const { foods } = useContext(FoodContext);
+
+  const toggleLocation = value => {
+    setLocation(value);
+  };
 
   return (
     <>
